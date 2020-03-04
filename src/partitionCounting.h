8b2dbfa9b9aef05a10b53d628dd4d9a1f070c820
Jean-Bernard Hayet <jbhayet@cimat.mx>
2020-03-04 13:30:41 -0600

    Pass the destination partition descriptor to avoid some recursions

--- src/partitionCounting.h
+++ src/partitionCounting.h
@@ -1,6 +1,7 @@
 // @author: jbhayet
 #include <list>
 #include <vector>
+#include "partitionDescriptor.h"
 
 // TODO: this value should be updated
 unsigned int l[180];
@@ -50,34 +51,33 @@ void ascPartition(unsigned int n, std::list<std::vector<unsigned int> > &listOfP
 // m: minimal value for the elements of the partition
 // p: maximal value for the elements of the partition
 // k: current number
-void recAscVariant(unsigned int n,unsigned int p,unsigned int m,unsigned int k,std::list<std::vector<unsigned int> >&listOfPartitions) {
+void recAscVariant(unsigned int n,unsigned int p,unsigned int m,unsigned int k,const partitionDescriptor &d,std::list<std::vector<unsigned int> >&listOfPartitions) {
   if (m<1) return;
   if (m>p) return;
   if (m>n) return;
   // Start at m
-  unsigned int x     = m;
   // Until x=p, tries all possible values of x (in increasing values)
-  while (x<=p) {
+  for (unsigned int x= m; x<=p; x++) if (d[x-1]>0) {
     // Get x into the description
     l[k]=x;
     // Get the partition of n-x, and will fill element k+1
     // TODO:
     if (x<=n)
-      recAscVariant(n-x,p,x,k+1,listOfPartitions);
-    // Increment x
-    x++;
+      recAscVariant(n-x,p,x,k+1,d,listOfPartitions);
   }
   // Here we cannot add more x's so we just add the remainder
   l[k]=n;
   // Just adds the obtained partition to the list
   if (k>1 && n<=p) {
     std::vector<unsigned int> newPartition(l+1,l+k+1);
+    //for (unsigned int i=1;i<=k;i++) std::cout << l[i] << " ";
+    //std::cout << std::endl;
     listOfPartitions.push_back(newPartition);
   }
 }
 
 // Main function for acendent partition
-void ascPartitionVariant(unsigned int n,unsigned int p, std::list<std::vector<unsigned int> > &listOfPartitions) {
-  recAscVariant(n,p,1,1,listOfPartitions);
+void ascPartitionVariant(unsigned int n,unsigned int p, const partitionDescriptor &d, std::list<std::vector<unsigned int> > &listOfPartitions) {
+  recAscVariant(n,p,1,1,d,listOfPartitions);
   return;
 }
